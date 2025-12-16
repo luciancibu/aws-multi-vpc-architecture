be7f13f31d60e844ca71cbc0649c8ad3268c8103
Lucian Cibu <luciancibu@yahoo.com>
2025-12-16 16:24:58 +0200

    Create security group for public ALB

---
+++ terraform/security.tf
@@ -0,0 +1,34 @@
+# ALB sg
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP/HTTPS from Internet"
+  vpc_id      = module.frontend_vpc.vpc_id
+
+  ingress {
+    description = "HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Outbound to frontend EC2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
