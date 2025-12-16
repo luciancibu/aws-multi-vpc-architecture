9f818c4a4c375aee913b49ddb0c67b89e37f86eb
Lucian Cibu <luciancibu@yahoo.com>
2025-12-16 18:29:23 +0200

    Security group fror backend and RDS

--- terraform/security.tf
+++ terraform/security.tf
@@ -48,15 +48,70 @@ resource "aws_security_group" "frontend_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Outbound to backend Flask"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
   }
-
 
   tags = {
     Name = "frontend-ec2-sg"
+  }
+}
+
+
+# Backend sg
+resource "aws_security_group" "backend_sg" {
+  name        = "backend-ec2-sg"
+  description = "Allow traffic from frontend only"
+  vpc_id      = module.backend_vpc.vpc_id
+
+  ingress {
+    description = "Flask from frontend VPC"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
+
+  egress {
+    description = "Outbound to RDS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]
+  }
+
+  tags = {
+    Name = "backend-ec2-sg"
+  }
+}
+
+# RDS sg
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow MySQL from backend EC2 only"
+  vpc_id      = module.backend_vpc.vpc_id
+
+  ingress {
+    description     = "MySQL from backend EC2"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  egress {
+    description     = "MySQL to backend EC2 only"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  tags = {
+    Name = "rds-sg"
   }
 }
 
